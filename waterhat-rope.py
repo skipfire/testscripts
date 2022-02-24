25cd9fac8f784ad375ee3ab0be7e71ebd63f21a7
Jon R. Helms <helmsjr@skipfire.com>
2022-02-24 09:18:23 -0600

    Water rope changes

--- waterhat-rope.py
+++ waterhat-rope.py
@@ -2,7 +2,6 @@
 #pip3 install adafruit-circuitpython-ads1x15
 
 from datetime import datetime
-from w1thermsensor import W1ThermSensor
 import time
 import board
 import busio
@@ -10,11 +9,7 @@ import adafruit_ads1x15.ads1015 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
 i2c = busio.I2C(board.SCL, board.SDA)
 ads = ADS.ADS1015(i2c)
-sensor = W1ThermSensor()
 while True:
-    temperature = sensor.get_temperature()
-    temperature = temperature * 1.8 + 32      
     chan = AnalogIn(ads, ADS.P0)
     print("ADC CH0: {:>5}\t{:>5.3f}".format(chan.value, chan.voltage))
-    print("Temperature: {:.1F}".format(temperature))
     time.sleep(0.5)
